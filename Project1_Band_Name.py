bccbd8f3882805cbc941f5367f06bedc7647f186
simer-wadhwa <simerwadhwa115@gmail.com>
2023-10-21 15:30:36 -0700

    First Project

---
+++ Project1_Band_Name.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band name Generator")
+print("What's the name of city you grow up in?")
+name = input()
+print("What's your pets name?")
+pet = input()
+print("Your Band name can be "+name+" "+pet)
